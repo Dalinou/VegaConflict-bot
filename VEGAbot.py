cdaaecf7aa6984a8a12193a1d9bd9c14ba8a4d56
Gwendal Braun <103582808+Dalinou@users.noreply.github.com>
2023-04-26 00:26:11 +0200

    Small editing of the code
    Translation of all comment from croatian to french and small edit in the code

--- VEGAbot.py
+++ VEGAbot.py
@@ -6,59 +6,60 @@ import numpy as np
 import random
 import win32api, win32con
 
-time.sleep(2)
+time.sleep(5)
+# code
+
 
 def click(x, y):
-    win32api.SetCursorPos((x,y))
+    win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(np.random.uniform(0.1, 0.3))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-    
-#bilo bi tesko ugasiti program s obzirom da mice mis cijelo vrijeme
-while keyboard.is_pressed('q') == False:
-    
-    # ODABIR FLOTE ZA KORISTENJE - biramo prvo random flotu, te nakon toga glavnu tako da popup window bude aktivan uvijek
+
+
+# il serait difficile d'arrêter le programme étant donné que la souris bouge tout le temps
+while not keyboard.is_pressed('q'):
+    # SÉLECTIONNER LA FLOTTE À UTILISER - nous sélectionnons d'abord la flotte aléatoire, puis la principale pour que
+    # la fenêtre contextuelle soit toujours active
     pyautogui.keyDown('1')
     time.sleep(np.random.uniform(0.1, 0.3))
     pyautogui.keyUp('1')
 
-   
     pyautogui.keyDown('3')
     time.sleep(np.random.uniform(0.2, 0.4))
     pyautogui.keyUp('3')
 
-    # cekanje između timera je da igrica teze skuzi da je ovo skripta
+    # attendre entre les minuteries, c'est pour que le jeu ne comprenne pas qu'il s'agit d'un script
     time.sleep(np.random.uniform(0.75, 1.25))
 
-    #PROVJERA - dali je flota u bitci 
+    # CHECK - est la flotte au combat
     if pyautogui.pixel(817, 1010)[0] != 232:
-        
-        #EVENT SPECIFIC - provjera za blitz popup    1436 Y:  188
-        if pyautogui.locateOnScreen('blitz.png', grayscale = True, confidence = 0.8) != None:
-            #gasi popup
-            click(1436, 188)
-            
-        #provjera ako slucajno napadamo s krivom flotom   X: 1056 Y:  664
-        if pyautogui.locateOnScreen('alert.png', grayscale = True, confidence = 0.7) != None:
-            #gasi popup
+
+        # SPÉCIFIQUE À L'ÉVÉNEMENT - vérifier la fenêtre contextuelle blitz 1436 Y : 188
+        # if pyautogui.locateOnScreen('blitz.png', grayscale=True, confidence=0.8) is not None:
+            # désactive la fenêtre contextuelle
+            # click(1436, 188)
+
+        # vérifier si nous attaquons accidentellement avec la mauvaise flotte X : 1056 Y : 664
+        if pyautogui.locateOnScreen('alert.png', grayscale=True, confidence=0.7) is not None:
+            # désactive la fenêtre contextuelle
             click(1056, 664)
-        
-        #FREE REPAIR - provjera
-       # if pyautogui.locateOnScreen('repair.png', grayscale = True, confidence = 0.8) != None:
-            #FREE REPAIR AVAILABLE
-       #     click(978, 928)
-            
+
+        # RÉPARATION GRATUITE - vérifier
+        # if pyautogui.locateOnScreen('repair.png', grayscale = True, confidence = 0.8) is not None:
+        # RÉPARATION GRATUITE DISPONIBLE
+        # clic(978, 928)
+
         time.sleep(np.random.uniform(1, 2))
 
-        #Klik na find fleet za napasti
+        # Cliquez sur trouver la flotte pour attaquer
         click(1627, 94)
 
-        #random wait
+        # random wait
         time.sleep(np.random.uniform(1, 1.5))
 
-        #PROVJERA BOJE PIKSELA BOTUNA ZA NAPAD
+        # VÉRIFICATION DE LA COULEUR DES PIXELS DU BOUTON D'ATTAQUE
         if pyautogui.pixel(817, 1010)[0] == 232:
-            #BOTUN CRVEN I SPREMAN ZA NAPASTI
+            # Bouton ROUGE ET PRÊT À ATTAQUER
             click(817, 1010)
             time.sleep(np.random.uniform(13, 15))
-
